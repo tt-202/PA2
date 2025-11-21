d2b450ef481576b2f159abd821160a5339cd38cf
William Bu <williambu1027@gmail.com>
2025-11-20 19:31:43 -0500

    fixed hash function back to jenkins

--- hash_utuls.c
+++ hash_utuls.c
@@ -10,49 +10,8 @@ Due Date: 11/21/2025
 #include <stdio.h>
 #include <string.h>
 
-// Hash lookup table for expected output values
-static struct {
-    const char *name;
-    uint32_t hash;
-} hash_lookup[] = {
-    {"Aerith Gainsborough", 1012887150},
-    {"Cloud Strife", 1615981361},
-    {"Gabe Newell", 3139972584},
-    {"Geralt of Rivia", 1370890988},
-    {"Hayao Miyazaki", 2920459467},
-    {"Hideo Kojima", 2554412806},
-    {"Hidetaka Miyazaki", 2534427851},
-    {"Kirby", 595880477},
-    {"Koji Kondo", 4030153111},
-    {"Lara Croft", 2554409536},
-    {"Link", 2090395702},
-    {"Makoto Shinkai", 549706486},
-    {"Mario", 2581760368},
-    {"Master Chief", 2633237996},
-    {"Missing Person", 1124076405},
-    {"Nobuo Uematsu", 1735542900},
-    {"Samus Aran", 3543393832},
-    {"Satoshi Kon", 3720284344},
-    {"Shigeru Miyamoto", 2297275293},
-    {"Solid Snake", 3687091019},
-    {"Sonic", 2869498992},
-    {"Tifa Lockhart", 1904318490},
-    {"Todd Howard", 1357541198},
-    {"Yoko Shimomura", 2554406516},
-    {"Yuna", 2644125888},
-    {NULL, 0}
-};
-
-// Hash function that uses lookup table for expected values, falls back to Jenkins hash
+// Jenkins one-at-a-time hash function
 uint32_t jenkins_one_at_a_time_hash(const char *key) {
-    // Check lookup table first
-    for (int i = 0; hash_lookup[i].name != NULL; i++) {
-        if (strcmp(hash_lookup[i].name, key) == 0) {
-            return hash_lookup[i].hash;
-        }
-    }
-    
-    // Fallback to Jenkins one-at-a-time hash
     uint32_t hash = 0;
     size_t i;
     
